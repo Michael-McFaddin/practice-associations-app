f65529e2d8b9dd385224d1a8170d09ee739a985f
mike <mikemcfaddin@gmail.com>
2020-02-03 23:10:53 -0500

    adds data via seeds file to all models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Meeting.create(
+  # title: "How to pour concrete", agenda: "We will go over some general properties of concrete and how to work with it.", location: "104 Broad St., Lavern, MI", time: "2:30pm"
+  # )
+# Meeting.create(
+#   title: "Working with insulation", agenda: "We will go over some general properties of fiberglass insulation and how to work with it.", location: "17980 Fort St., Dearborn, MI", time: "1:30pm"
+#   )
+# Meeting.create(
+#   title: "Where to source lumber", agenda: "We will discuss the who, what, when, where, and why of lumber sourcing.", location: "13 Trapper Lane., Fairmont, MI", time: "4:30pm"
+#   )
+# Meeting.create(
+#   title: "Proper tool belt fitting", agenda: "We will discuss the best ways to fit and wear a tool belt. Along with tool options.", location: "17290 Dan Dr.., Taylor, MI", time: "10:30am"
+#   )
+
+# Speaker.create(
+#   first_name: "Ted", last_name: "Stevens", email: "ted@somemail.com"
+#   )
+
+# Speaker.create(
+#   first_name: "Tina", last_name: "Kroop", email: "tinakroop@somemail.com"
+#   )
+# Speaker.create(
+#   first_name: "Brett", last_name: "Davis", email: "brettdavis@somemail.com"
+#   )
+# Speaker.create(
+#   first_name: "Mindy", last_name: "Morris", email: "mindymorris@somemail.com"
+#   )
+
+# SpeakerMeeting.create(
+#   speaker_id: 2, meeting_id: 4
+#   )
+SpeakerMeeting.create(
+  speaker_id: 1, meeting_id: 4
+  )
+SpeakerMeeting.create(
+  speaker_id: 3, meeting_id: 3
+  )
+SpeakerMeeting.create(
+  speaker_id: 2, meeting_id: 3
+  )
+SpeakerMeeting.create(
+  speaker_id: 1, meeting_id: 1
+  )
